54d84e1518abf6fe287594ff5e3ada828bd4351c
ana-clara <anaclara.aparecida.027@gmail.com>
2025-08-15 17:07:01 -0300

    update

---
+++ python_fundamentos/sobre o python/Hands-On.py
@@ -0,0 +1,12 @@
+# problema: apresentar uma saudaçao 'Ola, meu nome é XXXX,
+# -> tenho XX anos.'
+#1: capturar e armazenar a informaçao de nome 
+#2: capturar armazenar a informaçao de idade 
+#2: apresentar a mensagem de saudacao, utilizando as
+# -> informaçoes de nome e idade 
+
+# -> Codigo
+
+nome = input('Digite seu nome: ')
+idade = input('Informe a sua idade: ')
+print(f'Ola, meu nome é {nome}, e eu tenho {idade} anos!')
---
+++ python_fundamentos/sobre o python/Strings.py
@@ -0,0 +1,27 @@
+# Anotações ->
+
+"""2.3 strings
+' ou "
+
+-> frases
+
+\n -> pular de linha
+caso queira usar algum caractere que traga algum tipo de confusão, usar '\' barra invertida, neste caso ficaria '\\n'
+
+\t -> função do tab, tabeamento
+
+texto
+4 linux open software specialist
+texto[0] -> 4
+texto[1] ->
+texto[2] -> l
+texto[0:5] -> 4 lin 'o cinco não entra como o caractere, apenas onde vai parar'
+-> Já no caso de ocorrer 'texto[5]' ele já conta o caractere em si, no caso o 'u'
+texto[-1] -> t
+
+funções
+texto.count('4') -> 1
+texto.count('linux') -> 1
+texto.count('u', 0, 6) -> 1
+
+texto.strip() -> retira os espaços """
