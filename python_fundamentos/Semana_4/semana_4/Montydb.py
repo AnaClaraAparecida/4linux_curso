5efea02c046b8e8a08b519e9f234d5edf9da3890
Ana Clara Aparecida <anaclara.aparecida.027@gmail.com>
2025-10-15 17:49:43 -0300

    updates realizados essa semana

--- python_fundamentos/Semana_4/semana_4/Montydb.py
+++ python_fundamentos/Semana_4/semana_4/Montydb.py
@@ -1,3 +1,21 @@
+import montydb 
+
+#1: conectar com o banco 
+client = montydb.MontyClient('cafe')
+
+# 2: criar uma collection 
+db = client.ingrediente 
+
+#3: inserir um novo documento 
+doc = {
+    'nome': 'agua',
+    'descriçao': 'xpto',
+    'tipo': 'liquido',
+    'unidade_medida': 'ml'
+}
+
+db.ingrediente.insert_one(doc) 
+
 import montydb
 
 client = montydb.MontyClient('cafe')
@@ -10,3 +28,17 @@ for documento in db.ingrediente.find():
     print(documento)
 
 print(documento['nome'])
+
+
+import montydb 
+
+# conexao 
+client = montydb.MontyClient('cafe')
+
+# escolha o banco 
+db = client.ingrediente
+
+filtro = {'nome': 'cafe'}
+
+# acessar a collection e executa atualizaçao 
+db.ingrediente.update_one({'nome':'cafe'}, {'$set': {}})
