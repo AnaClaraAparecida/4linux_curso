9c15b6d03dbd4c9155d708616e9909ad8580ca65
Ana Clara Aparecida <anaclara.aparecida.027@gmail.com>
2025-10-30 18:28:20 -0300

    projeto caixa eletronico

--- python_fundamentos/projetos /caixa_eletronico /src/cliente.py
+++ python_fundamentos/projetos /caixa_eletronico /src/cliente.py
@@ -1,41 +1,52 @@
 from db import conectar
 from datetime import datetime
 
-conn = conectar()
-cursor = conn.cursor()
-
-def cunsultar_preco(id_usuario):
+def consultar_saldo(id_usuario):
+    conn = conectar()
+    cursor = conn.cursor()
+    id_usuario = input('Digite o ID do usuario: ')
+    print(f'Saldo atual: R${saldo}')
     cursor.execute('SELECT saldo FROM contas WHERE id_usuario = ?', (id_usuario,))
     saldo = cursor.fetchone()[0]
     conn.close()
     return saldo
 
 def deposito(id_usuario, valor):
-    cursor.execute('UPDATE contas SET saldo = saldo + ? WHERE id_usuario = ?', (valor, id_usuario))
-    cursor.registrar_transacao('deposito', valor, id_usuario, None)
-    conn.commit()
-    conn.close()
+    conn = conectar()
+    try:
+        cursor = conn.cursor()
+        cursor.execute('UPDATE contas SET saldo = saldo + ? WHERE id_usuario = ?', (valor, id_usuario))
+        registrar_transacao(cursor, 'depósito', valor, id_usuario, None)
+        conn.commit()
+    finally:
+        conn.close()
 
 def saque(id_usuario, valor):
+    conn = conectar()
+    cursor = conn.cursor()
     cursor.execute('SELECT saldo FROM contas WHERE id_usuario = ?', (id_usuario,))
-    saldo = cursor.fletchone()[0]
+    saldo = cursor.fetchone()[0]
     if saldo >= valor:
         cursor.execute('UPDATE contas SET saldo = saldo - ? WHERE id_usuario = ? ',(valor, id_usuario))
-        cursor.registrar_transacao('saque', valor, id_usuario, None)
+        registrar_transacao('saque', valor, id_usuario, None)
         conn.commit()
     conn.close()
 
 def transferencia(origem, destino, valor):
+    conn = conectar()
+    cursor = conn.cursor()
     cursor.execute('SELECT saldo FROM contas WHERE id_usuario = ?', (origem,))
     saldo = cursor.fletchone()[0]
     if saldo >= valor:
         cursor.execute('UPDATE contas SET saldo = saldo - ? WHERE id_usuario = ?', (valor, origem))
-        cursor.execute('UPDATE contas SET saldo = saldo - ? WHERE id_usuario = ?', (valor, destino))
-        cursor.registrar_transacao('trasferencia', valor, origem, destino)
+        cursor.execute('UPDATE contas SET saldo = saldo + ? WHERE id_usuario = ?', (valor, destino))
+        registrar_transacao('trasferencia', valor, origem, destino)
         conn.commit()
     conn.close()
 
 def registrar_transacao(tipo, valor, origem, destino):
+    conn = conectar()
+    cursor = conn.cursor()
     cursor.execute('INSERT INTO transacoes (tipo, valor, origem, destino, data) VALUES (?, ?, ?, ?, ?)', (tipo, valor, origem, destino, datetime.now().isoformat()))
     conn.commit()
     conn.close()
--- python_fundamentos/projetos /caixa_eletronico /src/menu.py
+++ python_fundamentos/projetos /caixa_eletronico /src/menu.py
@@ -4,14 +4,28 @@ import cliente
 print('~' * 15, 'CAIXA ELETRONICO', '~' * 15)
 
 def escolha():
-    print('Escolha a forma de login ')
+    while True:
+        print('Escolha a forma de login')
+        print(' 1) Cliente\n 2) Administraçao\n 3) Sair')
+        resp = input()
+        if resp == '1':
+            print(menu_cliente())
+        elif resp == '2':
+            print(menu_admin())
+        elif resp == '3':
+            print('Encerrando programa...')
+            print('Volte sempre!!')
+            break
+        else:
+            print('Forma de login invalida')
 
 
 def menu_cliente():
-    print('~' * 15, '', '~' * 15)
-
+    print('~' * 15, 'MENU CLIENTE', '~' * 15)
     print(' 1) Consultar saldo\n 2) Depositar\n 3) Sacar\n 4) Transferir\n 5) Alterar senha\n 6) Sair')
-
+    resp = input()
+    if resp == '1':
+        consultar_saudo()
 
 def menu_admin():
     print('1) Criar cliente\n 2) Desbloquear cliente\n 3) Resetar senha\n 4) Sair')
